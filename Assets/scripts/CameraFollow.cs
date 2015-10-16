2be949052fb64c7e2b3b72b4d1691ee275542716
chaosed0 <chaosed0@gmail.com>
2015-10-15 22:38:07 -0400

    Add smoothing parameters to camera

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -5,18 +5,23 @@ public class CameraFollow : MonoBehaviour {
     public ShipMotor ship;
     public Transform followPoint;
     public Transform lookAt;
-    public float smoothTime = 0.1f;
+    public float thrustSmoothing = 0.1f;
+    public float rotationalSmoothing = 0.05f;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate() {
-        transform.position = Vector3.SmoothDamp(transform.position, followPoint.position, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, followPoint.position, ref velocity, thrustSmoothing);
 
         float angVel = 0.0f;
         Vector3 shipRotation = ship.getCurrentRotation();
         Vector3 up = Quaternion.Euler(shipRotation.x, shipRotation.y, shipRotation.z / 10.0f) * Vector3.up;
         Quaternion target = Quaternion.LookRotation(lookAt.position - transform.position, up);
         float angle = Quaternion.Angle(transform.rotation, target);
-        float lerp = Mathf.SmoothDamp(angle, 0.0f, ref angVel, 0.1f);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, 1.0f - lerp / angle);
+        float newAngle = Mathf.SmoothDamp(angle, 0.0f, ref angVel, rotationalSmoothing);
+        float lerp = 1.0f;
+        if (angle != 0) {
+            lerp = 1.0f - newAngle / angle;
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, 1.0f - lerp);
     }
 }
