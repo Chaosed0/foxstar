f928f559025da71baa964cb6afa22b022314744a
chaosed0 <chaosed0@gmail.com>
2015-10-31 12:19:50 -0400

    Fix the minimap positioning when there are 2 players

--- Assets/scripts/ui/MinimapPositioner.cs
+++ Assets/scripts/ui/MinimapPositioner.cs
@@ -5,12 +5,15 @@ public class MinimapPositioner : MonoBehaviour {
     private int numPlayers = 0;
     private RectTransform minimap;
     private CanvasGroup minimapGroup;
+    private RectTransform parentTransform;
+
     public PlayerJoinEventer eventer;
     public bool verticalSplit = false;
 
 	void Start () {
         minimap = GetComponent<RectTransform>();
         minimapGroup = GetComponent<CanvasGroup>();
+        parentTransform = transform.parent.GetComponent<RectTransform>();
         eventer.OnPlayerJoined += OnPlayerJoined;
 	}
 
@@ -24,8 +27,8 @@ public class MinimapPositioner : MonoBehaviour {
     void positionAtCenterRight() {
         minimap.anchorMin = new Vector2(0.0f, 0.5f);
         minimap.anchorMax = new Vector2(0.0f, 0.5f);
-        minimap.pivot = new Vector2(0.0f, 0.5f);
-        minimap.anchoredPosition = new Vector2(minimap.anchoredPosition.x, 0.0f);
+        minimap.pivot = new Vector2(1.0f, 0.5f);
+        minimap.anchoredPosition = new Vector2(parentTransform.sizeDelta.x - minimap.anchoredPosition.x, 0.0f);
     }
 
     void positionAtCenter() {
@@ -37,18 +40,18 @@ public class MinimapPositioner : MonoBehaviour {
 
     void OnPlayerJoined(string playerPrefix) {
         /* For single-player, take initial minimap position */
-        if (numPlayers == 0) {
+        numPlayers++;
+        if (numPlayers == 1) {
             minimapGroup.alpha = 1.0f;
-        } else if (numPlayers == 1) {
+        } else if (numPlayers == 2) {
             if (verticalSplit) {
                 positionAtBottomCenter();
             } else {
                 positionAtCenterRight();
             }
-        } else if (numPlayers == 2) {
+        } else if (numPlayers == 3) {
             positionAtCenter();
         }
         /* For 4 players, just keep it in the center */
-        numPlayers++;
     }
 }
